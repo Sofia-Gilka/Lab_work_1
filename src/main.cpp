4897a50cca29083b602a6bc6532c684c57814f32
Sofia-Gilka <gilka.sofiya@mail.ru>
2025-05-12 17:15:33 +0300

    Initial commit: CMake project setup

---
+++ src/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "utils.h"
+
+int main() {
+    std::cout << "Amount: " << add(2, 3) << std::endl;
+    std::cout << "Difference: " << subtract(5, 2) << std::endl;
+    return 0;
+}
---
+++ src/utils.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+// Function to add two numbers
+int add(int a, int b) {
+    return a + b;
+}
+
+// Function to subtract one number from another
+int subtract(int a, int b) {
+    return a - b;
+}
