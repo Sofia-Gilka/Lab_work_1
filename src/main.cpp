a31c099644910707a4cda445327a8b914b16fe11
Sofia-Gilka <gilka.sofiya@mail.ru>
2025-05-12 17:18:28 +0300

    Add multiply function and use it in main

--- src/main.cpp
+++ src/main.cpp
@@ -4,5 +4,7 @@
 int main() {
     std::cout << "Amount: " << add(2, 3) << std::endl;
     std::cout << "Difference: " << subtract(5, 2) << std::endl;
+    std::cout << "Multiply: " << multiply(3, 4) << std::endl;
+
     return 0;
 }
