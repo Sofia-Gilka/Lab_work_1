9038f52b13fb6a3d9cadcace8c808bf271731952
Sofia-Gilka <gilka.sofiya@mail.ru>
2025-05-12 17:25:57 +0300

    fix: Update main output message in main branch

--- src/main.cpp
+++ src/main.cpp
@@ -4,5 +4,7 @@
 int main() {
     std::cout << "Amount: " << add(2, 3) << std::endl;
     std::cout << "Difference: " << subtract(5, 2) << std::endl;
+    std::cout << "Program finished!" << std::endl;
+
     return 0;
 }
